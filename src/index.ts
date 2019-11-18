ec2b568910d75d96930561b8b7ffea4a64c9d81f
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2019-11-18 17:57:01 +0100

    refactor: remove express mapping strategy and tests
    BREAKING CHANGE: `DefaultMappingStrategy` from `http-problem-details-mapper` or your own implementation of `IMappingStrategy` / `MappingStrategy` has to be used now.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
-import { ExpressMappingStrategy } from './ExpressMappingStrategy'
 import { HttpProblemResponse } from './HttpProblemResponse'
 
 export {
-  ExpressMappingStrategy,
   HttpProblemResponse
 }
